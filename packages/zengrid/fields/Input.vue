c2c97df29fc545c4e27263a543bf2be530f33a4d
YeFei <316606233@qq.com>
2022-01-10 18:01:31 +0800

    优化 input 事件

--- packages/zengrid/fields/Input.vue
+++ packages/zengrid/fields/Input.vue
@@ -1,19 +1,35 @@
 <template>
   <el-input
-    :value="value"
-    @input="input"
+    v-model="localValue"
     :maxlength="field.validate && field.validate.maxLength"
     :minlength="field.validate && field.validate.minLength"
     :show-word-limit="field.validate && field.validate.maxLength > 0"
     :placeholder="field.placeholder || field.label"
     clearable
     :size="size"
+    @change="input"
     suffix-icon="el-icon-search"
     style="width:200px;"
   />
 </template>
 
 <script>
-import basic from './basic';
-export default basic();
+export default {
+  props: ['field', 'value', 'size'],
+  data() {
+    return {
+      localValue: '',
+    }
+  },
+  watch: {
+    value() {
+      this.localValue = this.value;
+    }
+  },
+  methods: {
+    input() {
+      this.$emit('input', this.localValue);
+    }
+  }
+}
 </script>
